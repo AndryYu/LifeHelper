9cd907c8bbbbb91ba8dd2333c88cd112b09c32b7
andryyu <andryyu5419@163.com>
2017-03-12 13:07:18 +0800

    commit code

---
+++ app/src/main/java/com/android/andryyu/lifehelper/BaseApplication.java
@@ -0,0 +1,12 @@
+package com.android.andryyu.lifehelper;
+
+import android.app.Application;
+
+/**
+ * Created by yufei on 2017/3/12.
+ */
+
+public class BaseApplication extends Application {
+
+
+}
---
+++ mdsdk/src/main/java/com/android/andryyu/mdsdk/MDSDK.java
@@ -0,0 +1,10 @@
+package com.android.andryyu.mdsdk;
+
+/**
+ * Created by yufei on 2017/3/12.
+ */
+
+public class MDSDK {
+
+
+}
